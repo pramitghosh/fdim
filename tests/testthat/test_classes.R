476e3748c5071182f6cc6d65d50b0b1203174732
Pramit <32305898+pramitghosh@users.noreply.github.com>
2020-04-14 02:58:27 +0530

    Added tests for return classes of non-exported methods

--- tests/testthat/test_classes.R
+++ tests/testthat/test_classes.R
@@ -3,6 +3,12 @@ library('fdim')
 
 test_that(desc = "Importing sf",
             {
-              expect_is(st_is_longlat(fdim:::import_sf(dsn = system.file(package = "fdim"), layer = "madhya_pradesh")), FALSE)
+              expect_is(fdim:::import_sf(dsn = system.file(package = "fdim"), layer = "madhya_pradesh"), "sf")
+            }
+          )
+
+test_that(desc = "Overlaying grid",
+            {
+              expect_is(fdim:::overlay_grid(cs = 100000, f = fdim:::import_sf(dsn = system.file(package = "fdim"), layer = "madhya_pradesh")), "sfc_POLYGON")
             }
           )
