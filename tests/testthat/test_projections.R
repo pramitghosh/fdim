5d2afc94d1ea1bd0ab8be33d7ce369f083b04894
Pramit <32305898+pramitghosh@users.noreply.github.com>
2020-04-15 00:19:28 +0530

    check whether crs of incoming sf obj is long/lat

--- tests/testthat/test_projections.R
+++ tests/testthat/test_projections.R
@@ -1,11 +1,20 @@
 context("Projections\n")
 library('fdim')
 library('sf')
+library('rnaturalearth')
+
+deutschland = ne_countries(scale = "medium", country = "Germany", returnclass = "sf")
 
 test_that(desc = "Importing sf with long/lat CRS",
             {
               expect_equal(st_is_longlat(fdim:::import_sf(dsn = system.file(package = "fdim"), layer = "madhya_pradesh")), FALSE)
               expect_equal(st_crs(fdim:::import_sf(dsn = system.file(package = "fdim"), layer = "madhya_pradesh"))$input, "EPSG:3857")
+            }
+          )
+
+test_that(desc = "Reading sf object with long/lat CRS",
+            {
+              expect_output(bcd(deutschland, l = seq(25000, 100000, 25000)), "Note: Coordinates in Lat/Long; reprojecting to EPSG:3857...\n")
             }
           )
 
