3f2c0b431b88365909c46f779c5cd5618f3eb7e2
Pramit Ghosh <pramitghosh@outlook.com>
2024-08-11 19:43:01 +0200

    need to investigate cause of constant distance after a certain point of increase in t

--- R/other.R
+++ R/other.R
@@ -1,20 +1,36 @@
 
 similarity = function(p1, p2, plot = TRUE)
 {
-  p1_coords = st_transform(p1, crs = 32642) |> st_coordinates() 
-  p2_coords = st_transform(p2, crs = 32642) |> st_coordinates()
+  if(is.na(st_crs(p1)))
+  {
+    p1_coords = st_coordinates(p1)
+  } else
+  {
+    p1_coords = st_transform(p1, crs = 32642) |> st_coordinates()
+  }
+  
+  if(is.na(st_crs(p2)))
+  {
+    p2_coords = st_coordinates(p2)
+  } else
+  {
+    p2_coords = st_transform(p2, crs = 32642) |> st_coordinates()
+  }
+  # p1_coords = st_transform(p1, crs = 32642) |> st_coordinates() 
+  # p2_coords = st_transform(p2, crs = 32642) |> st_coordinates()
   
   p1_TA = as.data.frame(turning_angle(p1_coords))
   p2_TA = as.data.frame(turning_angle(p2_coords))
   
   alpha = diff_auc(p2_TA$px, p1_TA$px, p2_TA$py, p1_TA$py)
   
-  min_config = list(dist = alpha, NA, NA)
+  min_config = list(dist = -1)
   
+  all_points = list()
   for(i in 1:nrow(p2_coords))   #g
   {
     t_outer = p2_TA$px[i]
-    p_outer = shifter(p2_coords, i)
+    p_outer = shifter(p2_coords, i - 1)
     
     for(j in 1:nrow(p1_coords)) #f
     {
@@ -27,7 +43,7 @@ similarity = function(p1, p2, plot = TRUE)
       {
         t_net = 1 + t_net
       }
-      p_inner = shifter(p1_coords, j)
+      p_inner = shifter(p1_coords, j - 1)
       
       theta_optimal = alpha - (2 * pi * t_net)
       # print(paste("i = ", i, ", j = ", j, ", theta = ", theta_optimal, sep = ""))
@@ -35,7 +51,9 @@ similarity = function(p1, p2, plot = TRUE)
       dist_obj = dist_TA(p1_coords, p2_coords, theta_optimal)
       # print(dist_obj$dist)
       
-      if(dist_obj$dist <= min_config$dist)
+      all_points = append(all_points, list(c(dist = dist_obj$dist, i = i, j = j, theta = theta_optimal, t = t_net)))
+      
+      if(dist_obj$dist <= min_config$dist || min_config$dist < 0)
       {
         dist_obj = append(dist_obj, list(params = c(i = i, j = j, theta = theta_optimal, t = t_net)))
         min_config = dist_obj
@@ -54,6 +72,10 @@ similarity = function(p1, p2, plot = TRUE)
     plot_TA(p2_TA, col = "blue", add = TRUE)
     # par(mfrow = c(1, 1))
   }
+  
+  all_points_df = t(as.data.frame(all_points))
+  rownames(all_points_df) = 1:nrow(all_points_df)
+  min_config = append(min_config, list(df = all_points_df))
   
   return(min_config)
 }
--- R/other_similarity_utilities.R
+++ R/other_similarity_utilities.R
@@ -1,51 +1,55 @@
 poly_angles = function(p_coords)
 {
   coords_df = as.data.frame(p_coords)[c('X', 'Y')]
-  tan_angle = mat.or.vec(dim(coords_df)[1], 6)
   
-  for(i in 1:dim(coords_df)[1])
-  {
-    tan_angle[i, 1] = (coords_df$Y[i+1] - coords_df$Y[i])/(coords_df$X[i+1] - coords_df$X[i])
-    tan_angle[i, 2] = sign(coords_df$Y[i+1] - coords_df$Y[i])
-    tan_angle[i, 3] = sign(coords_df$X[i+1] - coords_df$X[i])
-  }
+  coords_df_false = rbind(coords_df, coords_df[2, ])
+  TrajAngles(TrajFromCoords(coords_df_false))
   
-  # tan_angle[dim(coords_df)[1], 1] = (coords_df$Y[1] - coords_df$Y[dim(coords_df)[1]])/(coords_df$X[1] - coords_df$X[dim(coords_df)[1]])
-  # tan_angle[dim(coords_df)[1], 2] = sign(coords_df$Y[1] - coords_df$Y[dim(coords_df)[1]])
-  # tan_angle[dim(coords_df)[1], 3] = sign(coords_df$X[1] - coords_df$X[dim(coords_df)[1]])
-  
-  tan_angle[,4] = atan(tan_angle[,1]) #* 180 / pi
-  tan_angle = as.data.frame(tan_angle)
-  colnames(tan_angle) = c("tan_theta", "sign_dY", "sign_dX", "atan_degree", "quadrant_corrected", "turning_angle")
-  tan_angle = na.omit(tan_angle)
-  
-  for(i in 1:dim(tan_angle)[1])
-  {
-    if(tan_angle$sign_dY[i] < 0)
-    {
-      if(tan_angle$sign_dX[i] < 0)
-        tan_angle$quadrant_corrected[i] = tan_angle$atan_degree[i] + pi else
-          if(tan_angle$sign_dX[i] > 0)
-            tan_angle$quadrant_corrected[i] = tan_angle$atan_degree[i] + 2*pi
-    } else
-      if(tan_angle$sign_dY[i] > 0 & tan_angle$sign_dX[i] < 0)
-        tan_angle$quadrant_corrected[i] = tan_angle$atan_degree[i] + pi else
-          tan_angle$quadrant_corrected[i] = tan_angle$atan_degree[i]
-  }
-  
-  tan_angle$turning_angle[1] = tan_angle$quadrant_corrected[dim(tan_angle)[1]] - tan_angle$quadrant_corrected[1]
-  for(i in 2:dim(tan_angle)[1])
-  {
-    tan_angle$turning_angle[i] = tan_angle$quadrant_corrected[i-1] - tan_angle$quadrant_corrected[i]
-  }
-  # tan_angle$turning_angle[dim(tan_angle)[1]] = tan_angle$quadrant_corrected[1] - tan_angle$quadrant_corrected[dim(tan_angle)[1]]
-  
-  tan_angle$turning_angle = ifelse(tan_angle$turning_angle < -pi, tan_angle$turning_angle + 2*pi, tan_angle$turning_angle)
-  tan_angle$turning_angle = ifelse(tan_angle$turning_angle > pi, tan_angle$turning_angle - 2*pi, tan_angle$turning_angle)
-  
-  # plot(cumsum(tan_angle$naive_turning), type = 'b')
-  
-  tan_angle
+  # tan_angle = mat.or.vec(dim(coords_df)[1], 6)
+  # 
+  # for(i in 1:dim(coords_df)[1])
+  # {
+  #   tan_angle[i, 1] = (coords_df$Y[i+1] - coords_df$Y[i])/(coords_df$X[i+1] - coords_df$X[i])
+  #   tan_angle[i, 2] = sign(coords_df$Y[i+1] - coords_df$Y[i])
+  #   tan_angle[i, 3] = sign(coords_df$X[i+1] - coords_df$X[i])
+  # }
+  # 
+  # # tan_angle[dim(coords_df)[1], 1] = (coords_df$Y[1] - coords_df$Y[dim(coords_df)[1]])/(coords_df$X[1] - coords_df$X[dim(coords_df)[1]])
+  # # tan_angle[dim(coords_df)[1], 2] = sign(coords_df$Y[1] - coords_df$Y[dim(coords_df)[1]])
+  # # tan_angle[dim(coords_df)[1], 3] = sign(coords_df$X[1] - coords_df$X[dim(coords_df)[1]])
+  # 
+  # tan_angle[,4] = atan(tan_angle[,1]) #* 180 / pi
+  # tan_angle = as.data.frame(tan_angle)
+  # colnames(tan_angle) = c("tan_theta", "sign_dY", "sign_dX", "atan_degree", "quadrant_corrected", "turning_angle")
+  # tan_angle = na.omit(tan_angle)
+  # 
+  # for(i in 1:dim(tan_angle)[1])
+  # {
+  #   if(tan_angle$sign_dY[i] < 0)
+  #   {
+  #     if(tan_angle$sign_dX[i] < 0)
+  #       tan_angle$quadrant_corrected[i] = tan_angle$atan_degree[i] + pi else
+  #         if(tan_angle$sign_dX[i] > 0)
+  #           tan_angle$quadrant_corrected[i] = tan_angle$atan_degree[i] + 2*pi
+  #   } else
+  #     if(tan_angle$sign_dY[i] > 0 & tan_angle$sign_dX[i] < 0)
+  #       tan_angle$quadrant_corrected[i] = tan_angle$atan_degree[i] + pi else
+  #         tan_angle$quadrant_corrected[i] = tan_angle$atan_degree[i]
+  # }
+  # 
+  # tan_angle$turning_angle[1] = tan_angle$quadrant_corrected[dim(tan_angle)[1]] - tan_angle$quadrant_corrected[1]
+  # for(i in 2:dim(tan_angle)[1])
+  # {
+  #   tan_angle$turning_angle[i] = tan_angle$quadrant_corrected[i-1] - tan_angle$quadrant_corrected[i]
+  # }
+  # # tan_angle$turning_angle[dim(tan_angle)[1]] = tan_angle$quadrant_corrected[1] - tan_angle$quadrant_corrected[dim(tan_angle)[1]]
+  # 
+  # tan_angle$turning_angle = ifelse(tan_angle$turning_angle < -pi, tan_angle$turning_angle + 2*pi, tan_angle$turning_angle)
+  # tan_angle$turning_angle = ifelse(tan_angle$turning_angle > pi, tan_angle$turning_angle - 2*pi, tan_angle$turning_angle)
+  # 
+  # # plot(cumsum(tan_angle$naive_turning), type = 'b')
+  # 
+  # tan_angle
 }
 
 
@@ -80,8 +84,10 @@ turning_angle = function(p_coords)
 {
   x = cumsum(poly_distances(p_coords))
   
-  y = poly_angles(p_coords)$turning_angle
-  y = c(y, y[1]) |> cumsum()
+  # x = x[2:length(x)]
+  
+  y = poly_angles(p_coords)
+  y = c(0, y) |> cumsum()
   
   # if(plot)
   # {
@@ -100,22 +106,33 @@ plot_TA = function(p, ylim = NULL, col = "black", add = FALSE)
   
   if(add == FALSE)
   {
-    plot(x = c(x, 1), y = c(y[1], y), type = 's', yaxt = "none", xlab = "Normalised perimeter", ylab = "Turning Angle", col = col, ylim = ylim)
-    breaks_pi = pretty(range(c(y[1], y)/pi))
+    plot(x = x, y = y, type = 's', yaxt = "none", xlab = "Normalised perimeter", ylab = "Turning Angle", col = col, ylim = ylim)
+    breaks_pi = pretty(range(y)/pi)
     axis(2, at = breaks_pi * pi, labels = paste(breaks_pi, "\u03c0"))
   } else
   {
-    lines(x = c(x, 1), y = c(y[1], y), type = 's', col = col)
+    lines(x = x, y = y, type = 's', col = col)
   }
 }
 
 
-diff_auc = function(x1_vals, x2_vals = x1_vals, y1_vals, y2_vals, false_offset = min(c(y1_vals, y2_vals)) + 1)
+diff_auc = function(x1_vals, x2_vals = x1_vals, y1_vals, y2_vals, false_offset = min(c(y1_vals, y2_vals)))
 {
+  if(false_offset < 0)
+  {
+    false_offset = 1 - false_offset
+  } else
+  {
+    false_offset = 0
+  }
+  
   # print(false_offset)
   a1 = AUC(x1_vals, y1_vals + false_offset, method = "step")
   a2 = AUC(x2_vals, y2_vals + false_offset, method = "step")
+  # print(paste("a1 = ", a1, ", a2 = ", a2))
   return(abs(a1 - a2))
+  
+  # AUC(x1_vals, abs(y2_vals - y1_vals))
 }
 
 merge_x_fill_y = function(p1_TA, p2_TA)
@@ -126,6 +143,29 @@ merge_x_fill_y = function(p1_TA, p2_TA)
   merged_df = merge(df1, df2, by = "px", sort = TRUE, all = TRUE)
   colnames(merged_df) = c('x_vals', 'y1_vals', 'y2_vals')
   
+  duplicated_xs = which(diff(merged_df$x_vals) < 1e-10)
+  if(length(duplicated_xs) > 0)
+  {
+    for(i in 1:length(duplicated_xs))
+    {
+      flag = FALSE
+      row_num = duplicated_xs[i]
+      df_subset = merged_df[row_num:(row_num+1), c('y1_vals', 'y2_vals')]
+      
+      if(is.na(df_subset[1,1]) && is.na(df_subset[2,2]) && abs(df_subset[1,2] - df_subset[2,1]) < 1e-10)
+      {
+        merged_df$x_vals[row_num] = -1
+        merged_df$y2_vals[row_num + 1] = df_subset[1,2]
+      } else
+        if(is.na(df_subset[1,2]) && is.na(df_subset[2,1]) && abs(df_subset[1,1] - df_subset[2,2]) < 1e-10)
+        {
+          merged_df$x_vals[row_num + 1] = -1
+          merged_df$y2_vals[row_num] = df_subset[2,2]
+        }
+    }
+    merged_df = merged_df[merged_df$x_vals > -1, ]
+  }
+  
   harmonised_df = tidyr::fill(merged_df, c(y1_vals, y2_vals), .direction = "downup")
   
   return(harmonised_df)
@@ -133,6 +173,11 @@ merge_x_fill_y = function(p1_TA, p2_TA)
 
 dist_TA = function(p1, p2, p1_theta)
 {
+  if(p1_theta < 1e-10)
+  {
+    p1_theta = 0
+  }
+  
   p1_TA = turning_angle(p1)
   p2_TA = turning_angle(p2)
   
@@ -142,5 +187,5 @@ dist_TA = function(p1, p2, p1_theta)
   
   dist = diff_auc(x1_vals = harmonised_df$x_vals, y1_vals = harmonised_df$y1_vals, y2_vals = harmonised_df$y2_vals)
   
-  return(list(dist = dist, p1_TA = p1_TA, p2_TA = p2_TA))
+  return(list(dist = dist, p1_TA = p1_TA, p2_TA = p2_TA, h_df = harmonised_df))
 }
